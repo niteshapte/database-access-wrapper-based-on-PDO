c00fa93a453b1d648a85321c48eb1ad13d047733
Nitesh Apte <niteshapte@users.noreply.github.com>
2014-10-18 20:03:35 +0200

    Values for database

---
+++ DBConfig.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Values for database
+ *
+ * @author Nitesh Apte
+ * @copyright 2014 Nitesh Apte
+ * @version 1.0
+ * @license GPL Version 3.0
+ */
+
+
+/**
+ * Database Connectivity Settings
+ */
+define('DBHOST', 'localhost');
+define('DBUSER', 'user');
+define('DBPASS', 'pass');
+define('DBNAME', 'databaseName');
+define('DBPORT', 3306);
+
+/**
+ * Choose Database
+ */
+define('DBTYPE', 'mysql');
+?>
